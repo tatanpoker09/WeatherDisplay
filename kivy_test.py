577088507f50cb03dde8d5c024e0cf98f592784d
Christian Eilers <christian@eilers.cl>
2023-07-16 00:49:48 -0700

    Refactored code

--- kivy_test.py
+++ kivy_test.py
@@ -1,39 +1,24 @@
 import asyncio
-from kivy.lang import Builder
+
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
-from kivy.uix.label import Label
 
 from weather import getweather
 
-Builder.load_string('''
-<MyWidget>:
-    orientation: 'vertical'
-    RelativeLayout:
-        Label:
-            text: 'Mountain View'
-            size_hint: None, None
-            size: root.width * 0.8, root.height * 0.1
-            pos_hint: {'center_x': 0.5, 'center_y': 0.25}
-            
-        Image:
-            source: 'resources/Sun.png'
-            size_hint: None, None
-            size: root.height * 0.7, root.height * 0.7
-            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-            
-        Label:
-            id: temperature_label
-            size_hint: None, None
-            size: root.width * 0.5, root.height * 0.1
-            pos_hint: {'center_x': 0.5, 'center_y': 0.15}
-''')
+
+def one_hour_in_seconds():
+    return 3600
+
 
 class MyWidget(BoxLayout):
     def __init__(self, **kwargs):
         super(MyWidget, self).__init__(**kwargs)
+        self.update_weather()  # Initial weather update
+        Clock.schedule_interval(self.update_weather, one_hour_in_seconds())  # Schedule weather update every 1 hour
 
+    def update_weather(self, dt=None):
         weather = asyncio.run(getweather('Mountain View'))
         self.ids.temperature_label.text = str(weather.current.temperature)
 
@@ -44,4 +29,5 @@ class MyApp(App):
 
 
 if __name__ == '__main__':
+    Builder.load_file("mywidget.kv")
     MyApp().run()
